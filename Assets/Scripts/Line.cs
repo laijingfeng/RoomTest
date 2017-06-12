c787a3d204c6f9ad424c4208ab778a5949eb0ad3
laijingfeng <337292616@qq.com>
2017-06-12 19:51:08 +0800

    update

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -3,36 +3,22 @@ using Jerry;
 
 public class Line : SingletonMono<Line>
 {
-    public Material mat;
-    public Color color = Color.red;
-    public Vector3 pos1;
-    public Vector3 pos2;
-
     [Range(0, 6)]
     public int m_WallCnt = 0;
     public bool m_Floor = false;
 
+    public Transform m_FloorGrid;
+    public Transform[] m_WallGrid;
+
     void Start()
     {
-        mat.color = color;
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            pos1 = Input.mousePosition;
-        }
-        if (Input.GetMouseButton(0))
-        {
-            pos2 = Input.mousePosition;
-        }
     }
 
     public void HideGrid()
     {
         m_Floor = false;
         m_WallCnt = 0;
+        RefreshGrid();
     }
 
     public void ShowGrid(MapUtil.SetType setType, int par = 6)
@@ -55,99 +41,25 @@ public class Line : SingletonMono<Line>
                 }
                 break;
         }
+        RefreshGrid();
     }
 
-    private Vector3 pos;
-
-    void OnPostRender()
+    private void RefreshGrid()
     {
-        GL.PushMatrix();
-        mat.SetPass(0);
-        GL.LoadOrtho();
-        GL.Begin(GL.LINES);
-        GL.Color(color);
-
-        //GL.Vertex3(pos1.x / Screen.width, pos1.y / Screen.height, 0);
-        //GL.Vertex3(pos2.x / Screen.width, pos2.y / Screen.height, 0);
-
-        if (m_WallCnt > 0)
+        if (m_FloorGrid != null)
         {
-            for (int i = 0; i <= m_WallCnt; i++)
+            m_FloorGrid.gameObject.SetActive(m_Floor);
+        }
+        
+        if (m_WallGrid != null)
+        {
+            for (int i = 0; i < 6; i++)
             {
-                pos = MapUtil.GetMap(Enum_Layer.Wall).m_StartPos + new Vector3(0, MapUtil.m_MapGridUnityLen * i, 0);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(MapUtil.GetMap(Enum_Layer.Wall).m_Size.x * MapUtil.m_MapGridUnityLen, 0, 0);
-                GL.Vertex(PosWorld2GL(pos));
-
-                pos = MapUtil.GetMap(Enum_Layer.LeftWall).m_StartPos + new Vector3(0, MapUtil.m_MapGridUnityLen * i, 0);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(0, 0, MapUtil.GetMap(Enum_Layer.LeftWall).m_Size.z * MapUtil.m_MapGridUnityLen);
-                GL.Vertex(PosWorld2GL(pos));
-
-                pos = MapUtil.GetMap(Enum_Layer.RightWall).m_StartPos + new Vector3(0, MapUtil.m_MapGridUnityLen * i, 0);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(0, 0, MapUtil.GetMap(Enum_Layer.RightWall).m_Size.z * MapUtil.m_MapGridUnityLen);
-                GL.Vertex(PosWorld2GL(pos));
-            }
-
-            for (int i = 0; i <= MapUtil.GetMap(Enum_Layer.Wall).m_Size.x; i++)
-            {
-                pos = MapUtil.GetMap(Enum_Layer.Wall).m_StartPos + new Vector3(MapUtil.m_MapGridUnityLen * i, 0, 0);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(0, m_WallCnt * MapUtil.m_MapGridUnityLen, 0);
-                GL.Vertex(PosWorld2GL(pos));
-            }
-
-            for (int i = 0; i <= MapUtil.GetMap(Enum_Layer.LeftWall).m_Size.z; i++)
-            {
-                pos = MapUtil.GetMap(Enum_Layer.LeftWall).m_StartPos + new Vector3(0, 0, MapUtil.m_MapGridUnityLen * i);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(0, m_WallCnt * MapUtil.m_MapGridUnityLen, 0);
-                GL.Vertex(PosWorld2GL(pos));
-            }
-
-            for (int i = 0; i <= MapUtil.GetMap(Enum_Layer.RightWall).m_Size.z; i++)
-            {
-                pos = MapUtil.GetMap(Enum_Layer.RightWall).m_StartPos + new Vector3(0, 0, MapUtil.m_MapGridUnityLen * i);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(0, m_WallCnt * MapUtil.m_MapGridUnityLen, 0);
-                GL.Vertex(PosWorld2GL(pos));
+                if (i < m_WallGrid.Length)
+                {
+                    m_WallGrid[i].gameObject.SetActive(m_WallCnt > i);
+                }
             }
         }
-
-        if (m_Floor)
-        {
-            for (int i = 0; i <= MapUtil.GetMap(Enum_Layer.FloorWall).m_Size.z; i++)
-            {
-                pos = MapUtil.GetMap(Enum_Layer.FloorWall).m_StartPos + new Vector3(0, 0, MapUtil.m_MapGridUnityLen * i);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(MapUtil.GetMap(Enum_Layer.FloorWall).m_Size.x * MapUtil.m_MapGridUnityLen, 0, 0);
-                GL.Vertex(PosWorld2GL(pos));
-            }
-
-            for (int i = 0; i <= MapUtil.GetMap(Enum_Layer.FloorWall).m_Size.x; i++)
-            {
-                pos = MapUtil.GetMap(Enum_Layer.FloorWall).m_StartPos + new Vector3(MapUtil.m_MapGridUnityLen * i, 0, 0);
-                GL.Vertex(PosWorld2GL(pos));
-                pos += new Vector3(0, 0, MapUtil.GetMap(Enum_Layer.FloorWall).m_Size.z * MapUtil.m_MapGridUnityLen);
-                GL.Vertex(PosWorld2GL(pos));
-            }
-        }
-
-        GL.End();
-        GL.PopMatrix();
-    }
-
-    private Vector3 PosWorld2GL(Vector3 pos)
-    {
-        return ScreenPosNormalize(Camera.main.WorldToScreenPoint(pos));
-    }
-
-    private Vector3 ScreenPosNormalize(Vector3 pos)
-    {
-        pos.z = 0;
-        pos.x /= Screen.width;
-        pos.y /= Screen.height;
-        return pos;
     }
 }
